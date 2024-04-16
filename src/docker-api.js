5e9a5cc6cac108f5eb765e3daaab1f78e9ce3d55
Emma Sax <sax.emma.h@gmail.com>
2024-04-16 15:27:47 -0500

    Try this

--- src/docker-api.js
+++ src/docker-api.js
@@ -8,14 +8,45 @@ const createDockerAPIClient = () => {
   return client;
 }
 
-const dockerAPIGet = (client, token, owner, container) => async (resource) => {
-  const headers = {
-    Authorization: `Bearer ${token}`
-  };
+// const dockerAPIGet = (client, token, owner, container) => async (resource) => {
+//   const headers = {
+//     Authorization: `Bearer ${token}`
+//   };
 
-  const url = `https://ghcr.io/v2/${owner}/${container}/${resource}`;
+//   const url = `https://ghcr.io/v2/${owner}/${container}/${resource}`;
 
-  const response = await client.get(url, { headers });
+//   const response = await client.get(url, { headers });
+
+//   if (response.message.statusCode != 200) {
+//     throw new Error(`Docker API request at ${url} was not successful. Status code: ${response.message.statusCode}, Status message: ${response.message.statusMessage}`);
+//   }
+
+//   return response;
+// }
+
+const dockerAPIGet = async (client, token, owner, container, resource) => {
+  // First, make an anonymous request to get the Www-Authenticate header
+  let response = await client.get(`https://ghcr.io/v2/${owner}/${container}/${resource}`);
+  const authenticateHeader = response.headers['www-authenticate'];
+
+  // Parse the Www-Authenticate header to get the realm, service, and scope
+  const match = /Bearer realm="(.*?)",service="(.*?)",scope="(.*?)"/.exec(authenticateHeader);
+  const [, realm, service, scope] = match;
+
+  // Use the realm, service, and scope to get a token
+  response = await client.get(`${realm}?service=${service}&scope=${scope}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  const { token: dockerToken } = await response.json();
+
+  // Use the token to make the authenticated request
+  response = await client.get(`https://ghcr.io/v2/${owner}/${container}/${resource}`, {
+    headers: {
+      Authorization: `Bearer ${dockerToken}`
+    }
+  });
 
   if (response.message.statusCode != 200) {
     throw new Error(`Docker API request at ${url} was not successful. Status code: ${response.message.statusCode}, Status message: ${response.message.statusMessage}`);
