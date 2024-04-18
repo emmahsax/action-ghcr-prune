a350261cdb349b4e32360dd20057d1782066fb39
Emma Sax <sax.emma.h@gmail.com>
2024-04-18 11:07:10 -0500

    Try to accept both manifest versions

--- src/docker-api.js
+++ src/docker-api.js
@@ -7,22 +7,57 @@ const createDockerAPIClient = () => {
   return client;
 }
 
-const dockerAPIGet = (client, token, owner, container) => async (resource) => {
-  const base64Token = Buffer.from(token).toString('base64');
+const dockerManifestV1 = (client, token, url) => async (resource) => {
   const headers = {
     Accept: `application/vnd.oci.image.index.v1+json`,
-    Authorization: `Bearer ${base64Token}`,
+    Authorization: `Bearer ${token}`,
   };
-
-  const url = `https://ghcr.io/v2/${owner}/${container}/${resource}`;
 
   const response = await client.get(url, headers);
 
   if (response.message.statusCode != 200) {
-    throw new Error(`Docker API request at ${url} was not successful`);
+    return {
+      success: false,
+      code: response.message.statusCode,
+      message: response.message.statusMessage
+    };
   }
 
-  return response;
+  return { success: true, resp: response };
+}
+
+const dockerManifestV2 = (client, token, url) => async (resource) => {
+  const headers = {
+    Accept: `application/vnd.docker.distribution.manifest.v2+json`,
+    Authorization: `Bearer ${token}`,
+  };
+
+  const response = await client.get(url, headers);
+
+  if (response.message.statusCode != 200) {
+    return {
+      success: false,
+      code: response.message.statusCode,
+      message: response.message.statusMessage
+    };
+  }
+
+  return { success: true, resp: response };
+}
+
+const dockerAPIGet = (client, token, owner, container) => async (resource) => {
+  const base64Token = Buffer.from(token).toString('base64');
+  const url = `https://ghcr.io/v2/${owner}/${container}/${resource}`;
+  responseV1 = dockerManifestV1(client, base64Token, url)(resource);
+  responseV2 = dockerManifestV2(client, base64Token, url)(resource);
+
+  if (responseV1['success']) {
+    return responseV1['resp'];
+  } else if (responseV2['success']) {
+    return responseV2['resp'];
+  } else {
+    throw new Error(`All Docker API requests at ${url} were unsuccessful. Docker manifest v1: status code - ${responseV1['code']}, status message - ${responseV1['message']}. Docker manifest v2: status code - ${responseV2['code']}, status message - ${responseV2['message']}`);
+  }
 }
 
 const getManifest = (getCmd) => async (tag) => {
